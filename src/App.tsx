22929eac910aa9481798f28d011d544ce00825fc
c-malecki <chrismmalecki@gmail.com>
2023-09-30 21:05:43 -0400

    removed context usage. wasn't really necessary for level of component depth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
-import { CalculatorProvider } from "./context/CalculatorContext";
 import Calculator from "./components/calculator/Calculator";
 
 function App() {
   return (
-    <CalculatorProvider>
-      <div className="main-container">
-        <h1>TitanStar Legends &#45; Rune Mastery Loadout Talent Calculator 9000</h1>
-        <Calculator />
-      </div>
-    </CalculatorProvider>
+    <div className="main-container">
+      <h1>TitanStar Legends &#45; Rune Mastery Loadout Talent Calculator 9000</h1>
+      <Calculator />
+    </div>
   );
 }
 
